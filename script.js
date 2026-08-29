ed66888ba6b492a88fc2868128e6843cca6a05d4
karinazhao2-ctrl <karinazhao2@gmail.com>
2026-08-29 15:35:33 -0700

    Worked on displaying inputs in results, and selecting buttons

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ pagesIdList = ["welcome", "q1", "q2", "q3", "q4", "q5", "q6", "results"];
 pagesLabelList = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Results!"];
 
 date = null;
-place = null;
-food = null;
-activity = null;
+place = "No option selected!";
+food = "No option selected!";
+activity = "No option selected!";
 notes = null;
 
 function startPlanner() {
@@ -48,6 +48,10 @@ function setQuestionVisible() {
         document.getElementById("progress-area").classList.add("hidden");
     }
 
+    if (currentPage == 7) {
+        generateResultsScreen();
+    }
+
     /* UNFINISHED
     date = new Date(document.getElementById("date-input").value + "T" + document.getElementById("time-input").value);
     console.log(document.getElementById("date-input").value);
@@ -57,10 +61,84 @@ function setQuestionVisible() {
 
 function pickPlace(element, label) {
     document.getElementById("place-other-input").classList.add("hidden");
-    placeObject = {};
-    place = placeObject[label];
+    place = label;
+
+    optionDivs = document.getElementById("q3").getElementsByClassName("option");
+    for (var i = 0; i < optionDivs.length; i++) {
+        optionDivs[i].classList.remove("picked");
+    }
+    element.classList.add("picked");
 }
 
-function pickOther(ummTestIguessidk) {
-    document.getElementById("place-other-input").classList.remove("hidden");
+function pickFood(element, label) {
+    document.getElementById("food-other-input").classList.add("hidden");
+    food = label;
+
+    optionDivs = document.getElementById("q4").getElementsByClassName("option");
+    for (var i = 0; i < optionDivs.length; i++) {
+        optionDivs[i].classList.remove("picked");
+    }
+    element.classList.add("picked");
+}
+
+function pickActivity(element, label) {
+    document.getElementById("activity-other-input").classList.add("hidden");
+    activity = label;
+
+    optionDivs = document.getElementById("q5").getElementsByClassName("option");
+    for (var i = 0; i < optionDivs.length; i++) {
+        optionDivs[i].classList.remove("picked");
+    }
+    element.classList.add("picked");
+}
+
+function pickOther(subject) {
+    document.getElementById(subject + "-other-input").classList.remove("hidden");
+
+    subjectsToId = {"place": "q3", "food": "q4", "activity": "q5"};
+    optionDivs = document.getElementById(subjectsToId[subject]).getElementsByClassName("option");
+    for (var i = 0; i < optionDivs.length; i++) {
+        optionDivs[i].classList.remove("picked");
+    }
+    document.getElementById(subject + "-other").classList.add("picked");
+}
+
+function typedOther(subject) {
+    console.log("Test!");
+    switch (subject) {
+        case "place":
+            place = document.getElementById("place-other-input").value;
+            break;
+        case "food":
+            food = document.getElementById("food-other-input").value;
+            break;
+        case "activity":
+            activity = document.getElementById("activity-other-input").value;
+            break;
+    }
+    
+}
+
+function generateResultsScreen() {
+
+
+    document.getElementById("results-place").innerText = place;
+    document.getElementById("results-food").innerText = food;
+    document.getElementById("results-activities").innerText = activity;
+
+    stuffToBring = "Phone\nWallet\nKeys\n";
+    placeSpecificItems = {
+        "Amusement park 🎢": "",
+        "Park 🏞️": "",
+        "Travel 🗺️": "Luggage\nPassport\n",
+        "Stay home 🏚️": "",
+        "Friend&rsquo;s house 🏘️": "Games"
+    };
+
+    if (document.getElementById("notes-input").value == "") {
+        document.getElementById("notes-area").classList.add("hidden");
+    } else {
+        document.getElementById("results-notes").innerText = document.getElementById("notes-input").value;
+        document.getElementById("notes-area").classList.remove("hidden");
+    }
 }
